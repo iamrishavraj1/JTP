38dc9fbbcc0acc81c73275e1164dc4a5c2b7801d
Rishav Raj <iamrishavraj1@gmail.com>
2022-10-16 01:25:50 +0530

    Head Done

---
+++ NamsteJS/Closure.js
@@ -0,0 +1,18 @@
+// function x(){
+//     var a = 25;
+//     function y(){
+//         console.log(a);
+//     }
+//     y();
+// }
+// x();
+
+function x(){
+    var a = 25;
+    function y(){
+        console.log(a);
+    }
+    return y;
+}
+var z = x();
+console.log(z);
